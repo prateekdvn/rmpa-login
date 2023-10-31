e97b28bf2ad1976f5cde52763dde4274db5a7e39
Dhawan <prateekdhawan@kpmg.com>
2023-10-31 12:25:28 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import bgImp from './images/login-backgrnd.png';
+import logo from './images/logo.png';
+import emp from './images/employee_logo.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <div className='employee-logo'>
+          <img src={emp}/>
+        </div>
+        <div className='container'>
+          <div className='left-col'>
+            <img src={bgImp} alt='' />
+          </div>
+          <div className='right-col'>
+            <div className='centered'>
+              <img src={logo} className='img' alt=''/>
+            </div>
+            <form id='form' className='flex flex-col'>
+              <h2 style={{fontWeight: 400}}>Login as a Dealer</h2>
+              <h3 style={{color: "darkblue", fontWeight: 400}}>Enter MSPIN</h3>
+              <input type="text" placeholder='*******' />
+              <button className='btn'>Sign In</button>
+            </form>
+          </div>
+        </div>
+      </>
   );
+
 }
 
 export default App;
